3022de7ca4c03aec871346cb06237a9de5e3b1b5
Bart Jacobs <developer@bartjacobs.com>
2016-09-27 16:34:47 +0200

    Refactor WeatherData to conform to JSONDecodable

--- DecodingJSONDataInSwift.playground/Sources/WeatherData.swift
+++ DecodingJSONDataInSwift.playground/Sources/WeatherData.swift
@@ -1,5 +1,3 @@
-import Foundation
-
 public struct WeatherData {
 
     public let lat: Double
@@ -17,25 +15,10 @@ public struct WeatherData {
 
 extension WeatherData: JSONDecodable {
 
-    public init?(JSON: Any) {
-        guard let JSON = JSON as? [String: AnyObject] else { return nil }
-
-        guard let lat = JSON["latitude"] as? Double else { return nil }
-        guard let long = JSON["longitude"] as? Double else { return nil }
-        guard let hourlyData = JSON["hourly"]?["data"] as? [[String: AnyObject]] else { return nil }
-
-        self.lat = lat
-        self.long = long
-
-        var buffer = [WeatherHourData]()
-
-        for hourlyDataPoint in hourlyData {
-            if let weatherHourData = WeatherHourData(JSON: hourlyDataPoint) {
-                buffer.append(weatherHourData)
-            }
-        }
-
-        self.hourData = buffer
+    public init(decoder: JSONDecoder) throws {
+        self.lat = try decoder.decode(key: "latitude")
+        self.long = try decoder.decode(key: "longitude")
+        self.hourData = try decoder.decode(key: "hourly.data")
     }
     
 }
